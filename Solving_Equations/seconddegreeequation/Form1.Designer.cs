226979d8e7af8a8919c0500a2792aea4b0612f6d
Elsayed Hamdy <elsaydhamdy2@gmail.com>
2020-02-05 04:21:14 +0200

    Solving Second Degree Equations.

---
+++ Solving_Equations/seconddegreeequation/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_xP1 = new System.Windows.Forms.TextBox();
+            this.lb_x = new System.Windows.Forms.Label();
+            this.txt_xP2 = new System.Windows.Forms.TextBox();
+            this.lb_y = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_r_x = new System.Windows.Forms.Label();
+            this.lb_r_y = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_solve = new System.Windows.Forms.Button();
+            this.txt_rx1 = new System.Windows.Forms.TextBox();
+            this.txt_rx2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_c = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_xP1
+            // 
+            this.txt_xP1.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_xP1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_xP1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_xP1.Location = new System.Drawing.Point(12, 12);
+            this.txt_xP1.Name = "txt_xP1";
+            this.txt_xP1.Size = new System.Drawing.Size(47, 33);
+            this.txt_xP1.TabIndex = 0;
+            this.txt_xP1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lb_x
+            // 
+            this.lb_x.AutoSize = true;
+            this.lb_x.Font = new System.Drawing.Font("SWComp", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_x.Location = new System.Drawing.Point(65, 12);
+            this.lb_x.Name = "lb_x";
+            this.lb_x.Size = new System.Drawing.Size(46, 35);
+            this.lb_x.TabIndex = 1;
+            this.lb_x.Text = "X²";
+            // 
+            // txt_xP2
+            // 
+            this.txt_xP2.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_xP2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_xP2.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_xP2.Location = new System.Drawing.Point(154, 13);
+            this.txt_xP2.Name = "txt_xP2";
+            this.txt_xP2.Size = new System.Drawing.Size(47, 33);
+            this.txt_xP2.TabIndex = 0;
+            this.txt_xP2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lb_y
+            // 
+            this.lb_y.AutoSize = true;
+            this.lb_y.Font = new System.Drawing.Font("SWComp", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lb_y.Location = new System.Drawing.Point(207, 13);
+            this.lb_y.Name = "lb_y";
+            this.lb_y.Size = new System.Drawing.Size(35, 35);
+            this.lb_y.TabIndex = 1;
+            this.lb_y.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("3ds", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(117, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 33);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "+";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("3ds", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(332, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "=";
+            // 
+            // lb_r_x
+            // 
+            this.lb_r_x.AutoSize = true;
+            this.lb_r_x.Font = new System.Drawing.Font("SWComp", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lb_r_x.Location = new System.Drawing.Point(13, 53);
+            this.lb_r_x.Name = "lb_r_x";
+            this.lb_r_x.Size = new System.Drawing.Size(35, 35);
+            this.lb_r_x.TabIndex = 1;
+            this.lb_r_x.Text = "X";
+            // 
+            // lb_r_y
+            // 
+            this.lb_r_y.AutoSize = true;
+            this.lb_r_y.Font = new System.Drawing.Font("SWComp", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lb_r_y.Location = new System.Drawing.Point(13, 90);
+            this.lb_r_y.Name = "lb_r_y";
+            this.lb_r_y.Size = new System.Drawing.Size(35, 35);
+            this.lb_r_y.TabIndex = 1;
+            this.lb_r_y.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("3ds", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("3ds", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(54, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 33);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "=";
+            // 
+            // btn_solve
+            // 
+            this.btn_solve.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_solve.FlatAppearance.BorderSize = 0;
+            this.btn_solve.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGray;
+            this.btn_solve.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.btn_solve.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_solve.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_solve.Location = new System.Drawing.Point(319, 79);
+            this.btn_solve.Name = "btn_solve";
+            this.btn_solve.Size = new System.Drawing.Size(75, 44);
+            this.btn_solve.TabIndex = 3;
+            this.btn_solve.Text = "Solve";
+            this.btn_solve.UseVisualStyleBackColor = true;
+            this.btn_solve.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_rx1
+            // 
+            this.txt_rx1.BackColor = System.Drawing.SystemColors.Control;
+            this.txt_rx1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_rx1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_rx1.Location = new System.Drawing.Point(91, 58);
+            this.txt_rx1.Name = "txt_rx1";
+            this.txt_rx1.ReadOnly = true;
+            this.txt_rx1.Size = new System.Drawing.Size(47, 26);
+            this.txt_rx1.TabIndex = 4;
+            this.txt_rx1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_rx1.TextChanged += new System.EventHandler(this.txt_rx1_TextChanged);
+            // 
+            // txt_rx2
+            // 
+            this.txt_rx2.BackColor = System.Drawing.SystemColors.Control;
+            this.txt_rx2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_rx2.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_rx2.Location = new System.Drawing.Point(91, 91);
+            this.txt_rx2.Name = "txt_rx2";
+            this.txt_rx2.ReadOnly = true;
+            this.txt_rx2.Size = new System.Drawing.Size(47, 26);
+            this.txt_rx2.TabIndex = 4;
+            this.txt_rx2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("3ds", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(242, 14);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 33);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "+";
+            // 
+            // txt_c
+            // 
+            this.txt_c.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_c.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_c.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_c.Location = new System.Drawing.Point(279, 13);
+            this.txt_c.Name = "txt_c";
+            this.txt_c.Size = new System.Drawing.Size(47, 33);
+            this.txt_c.TabIndex = 0;
+            this.txt_c.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("SWComp", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(369, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 35);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "0";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 127);
+            this.Controls.Add(this.txt_rx2);
+            this.Controls.Add(this.txt_rx1);
+            this.Controls.Add(this.btn_solve);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lb_y);
+            this.Controls.Add(this.txt_c);
+            this.Controls.Add(this.txt_xP2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lb_r_y);
+            this.Controls.Add(this.lb_r_x);
+            this.Controls.Add(this.lb_x);
+            this.Controls.Add(this.txt_xP1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form3";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_xP1;
+        private System.Windows.Forms.Label lb_x;
+        private System.Windows.Forms.TextBox txt_xP2;
+        private System.Windows.Forms.Label lb_y;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_r_x;
+        private System.Windows.Forms.Label lb_r_y;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_solve;
+        private System.Windows.Forms.TextBox txt_rx1;
+        private System.Windows.Forms.TextBox txt_rx2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_c;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Solving_Equations/seconddegreeequation/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public double[] varaibles;
+        public Form1()
+        {
+            InitializeComponent();
+            varaibles = new double[3];
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (validate(ref varaibles))
+            {
+                if (varaibles[0] != 0)
+                {
+                    double root = Math.Pow(varaibles[1], 2) - 4 * varaibles[0] * varaibles[2];
+                    if (root == Math.Abs(root))
+                    {
+                        txt_rx2.Text = ((-varaibles[1] - Math.Sqrt(root)) / (2 * varaibles[0])).ToString();
+                        txt_rx1.Text = ((-varaibles[1] + Math.Sqrt(root)) / (2 * varaibles[0])).ToString();
+                    }
+                    else
+                    {
+                        txt_rx2.Text = (-varaibles[1] / (2 * varaibles[0])).ToString() + " - " + (Math.Sqrt(-root) / (2 * varaibles[0])).ToString() + "i";
+                        txt_rx1.Text = (-varaibles[1] / (2 * varaibles[0])).ToString() + " + " + (Math.Sqrt(-root) / (2 * varaibles[0])).ToString() + "i";
+                    }
+                }
+                else
+                {
+                    txt_rx2.Text = "";
+                    txt_rx1.Text = (-varaibles[2] / varaibles[1]).ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Syntax Error");
+            }
+        }
+        public bool validate (ref double[]variables)
+            {
+            if (double.TryParse(txt_xP1.Text, out varaibles[0])) ;
+            else return false;
+            if (double.TryParse(txt_xP2.Text, out varaibles[1])) ;
+            else return false;
+            if (double.TryParse(txt_c.Text, out varaibles[2])) ;
+            else return false;
+            return true;
+            }
+        private void txt_rx1_TextChanged(object sender, EventArgs e)
+        {
+            txt_rx2.Size = TextRenderer.MeasureText(txt_rx2.Text, txt_rx2.Font);
+            txt_rx1.Size = TextRenderer.MeasureText(txt_rx1.Text, txt_rx1.Font);
+            if (txt_rx1.Location.X + txt_rx1.Size.Width >= btn_solve.Location.X || txt_rx2.Location.X + txt_rx2.Size.Width >= btn_solve.Location.X)
+            {
+                if (txt_rx1.Size.Width >= txt_rx2.Size.Width) this.Width += (txt_rx1.Location.X + txt_rx1.Size.Width) - btn_solve.Location.X + 3;
+                else this.Width += (txt_rx2.Location.X + txt_rx2.Size.Width) - btn_solve.Location.X + 3;
+            }
+            else
+            {
+                if (txt_rx1.Size.Width >= txt_rx2.Size.Width) this.Width -= btn_solve.Location.X - (txt_rx1.Location.X + txt_rx1.Size.Width);
+                else this.Width -= btn_solve.Location.X - (txt_rx2.Location.X + txt_rx2.Size.Width);
+                if (this.Width < 420) this.Width = 420;
+            }
+        }
+    }
+}
