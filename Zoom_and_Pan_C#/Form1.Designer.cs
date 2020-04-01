ea8022a90c77e56b1112eb86e4f2833100f064fa
Elsayed Hamdy <elsaydhamdy2@gmail.com>
2020-04-01 21:10:38 +0200

    Adding Zoom and Pan by C#.

---
+++ Zoom_and_Pan_C#/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ImageViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 45);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(474, 45);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 371);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 45);
+            this.panel3.Margin = new System.Windows.Forms.Padding(0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(10, 371);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel6
+            // 
+            this.panel6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel6.Controls.Add(this.pictureBox1);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(10, 45);
+            this.panel6.Margin = new System.Windows.Forms.Padding(0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(464, 371);
+            this.panel6.TabIndex = 6;
+            this.panel6.SizeChanged += new System.EventHandler(this.panel6_SizeChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(163, 145);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // panel4
+            // 
+            this.panel4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel4.Controls.Add(this.trackBar1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 416);
+            this.panel4.Margin = new System.Windows.Forms.Padding(0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(484, 45);
+            this.panel4.TabIndex = 3;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(61, 0);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(0);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 10;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.panel6_SizeChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Zoom_and_Pan_C#/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace ImageViewer
+{
+    public partial class Form1 : Form
+    {
+        Bitmap orignal, work;
+        Point mouselocation; //gets the mouse location when the left mouse key pressed on the picturebox while scrollbars are visible
+        float aspect;
+        Point upperleftcorner //gets the upperleftcorner of the picturebox 
+        {
+            get
+            {
+                int x, y;
+                x = (panel6.ClientSize.Width - (work.Width * trackBar1.Value / 10)) / 2;
+                y = (panel6.ClientSize.Height - (work.Height * trackBar1.Value / 10)) / 2;
+                if (x < 0) x = 0;
+                if (y < 0) y = 0;
+                return new Point(x, y);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            orignal = new Bitmap(@".\test.jpg"); //width=height
+            //orignal = new Bitmap(@".\taylor-hernandez-DLKR_x3T_7s-unsplash.jpg"); //height>width
+            //orignal = new Bitmap(@".\Screenshot (2).png"); //height<width
+            aspect = (float)orignal.Width / orignal.Height; //the aspect ratio
+            if (Screen.PrimaryScreen.Bounds.Height < orignal.Height || Screen.PrimaryScreen.Bounds.Width < orignal.Width)
+            {
+                orignal = new Bitmap(orignal, Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height * aspect), Convert.ToInt32(Screen.PrimaryScreen.Bounds.Width / aspect));
+            }
+            work = Adjust(orignal); //fits the image inside the panel
+            mouselocation = new Point();
+            pictureBox1.Image = work; //shows the image in a picturebox
+            pictureBox1.Location = upperleftcorner; //sets the location of the picturebox inside the panel
+        }
+        /* Adjust the image size according to the panel size
+         * while keeping the ratio between width and height
+         */
+        Bitmap Adjust(Bitmap image)
+        {
+            Bitmap output = new Bitmap(image.Width, image.Height);
+            if (output.Width > panel6.ClientSize.Width || output.Height > panel6.ClientSize.Height)
+            {
+                if (output.Height > output.Width)
+                {
+                    if (output.Height > panel6.ClientSize.Height) output = new Bitmap(Convert.ToInt32(panel6.ClientSize.Height * aspect), panel6.ClientSize.Height);
+                    if (output.Width > panel6.ClientSize.Width) output = new Bitmap(panel6.ClientSize.Width, Convert.ToInt32(panel6.ClientSize.Width / aspect));
+                }
+                else if (output.Height < output.Width)
+                {
+                    if (output.Width > panel6.ClientSize.Width) output = new Bitmap(panel6.ClientSize.Width, Convert.ToInt32(panel6.ClientSize.Width / aspect));
+                    if (output.Height > panel6.ClientSize.Height) output = new Bitmap(Convert.ToInt32(panel6.ClientSize.Height * aspect), panel6.ClientSize.Height);
+                }
+                else
+                {
+                    if (output.Height > panel6.ClientSize.Height)
+                    {
+                        output = new Bitmap(panel6.ClientSize.Height, panel6.ClientSize.Height);
+                    }
+                    else if (output.Width > panel6.ClientSize.Width)
+                    {
+                        output = new Bitmap(panel6.ClientSize.Width, panel6.ClientSize.Width);
+                    }
+                    else
+                    {
+                        output = new Bitmap(panel6.ClientSize.Width, panel6.ClientSize.Height);
+                    }
+                }
+            }
+            output = new Bitmap(image, output.Width, output.Height);
+            return output;
+        }
+        /* everytime the panel's size changes the image size Adjusts according to the new size
+         * everytime the sliderbar value changes the image size will adjust to fit that change
+         */
+        private void panel6_SizeChanged(object sender, EventArgs e)
+        {
+            work = Adjust(orignal);
+            pictureBox1.Image = new Bitmap(work, new Size(work.Width * trackBar1.Value / 10, work.Height * trackBar1.Value / 10));
+            pictureBox1.Location = upperleftcorner;
+            if (pictureBox1.Image.Height > panel6.ClientSize.Height) panel6.AutoScroll = true;
+            if (pictureBox1.Image.Width > panel6.ClientSize.Width) panel6.AutoScroll = true;
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && (panel6.VerticalScroll.Visible == true || panel6.HorizontalScroll.Visible == true))
+            {
+                mouselocation = new Point(e.X, e.Y);
+            }
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && (panel6.VerticalScroll.Visible == true || panel6.HorizontalScroll.Visible == true))
+            {
+                int X = (mouselocation.X - e.X);
+                int Y = (mouselocation.Y - e.Y);
+                panel6.AutoScrollPosition = new Point((X - panel6.AutoScrollPosition.X),(Y - panel6.AutoScrollPosition.Y));
+            }
+        }
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            if (panel6.VerticalScroll.Visible == true || panel6.HorizontalScroll.Visible == true)
+            {
+                pictureBox1.Cursor = Cursors.Hand;
+            }
+        }
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.Cursor = Cursors.Default;
+        }
+    }
+}
