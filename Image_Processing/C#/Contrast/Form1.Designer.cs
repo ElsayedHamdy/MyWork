dccb5c681c70da3882feda354a1fc40ce3aa6999
Elsayed Hamdy <elsaydhamdy2@gmail.com>
2020-06-19 03:32:39 +0200

    Adding Contrast by C#.

---
+++ Image_Processing/C#/Contrast/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Brightness
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loadbtn = new System.Windows.Forms.Button();
+            this.convertbtn = new System.Windows.Forms.Button();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.updown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.updown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(693, 693);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loadbtn
+            // 
+            this.loadbtn.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadbtn.Location = new System.Drawing.Point(713, 13);
+            this.loadbtn.Name = "loadbtn";
+            this.loadbtn.Size = new System.Drawing.Size(75, 40);
+            this.loadbtn.TabIndex = 1;
+            this.loadbtn.Text = "Load";
+            this.loadbtn.UseVisualStyleBackColor = true;
+            this.loadbtn.Click += new System.EventHandler(this.loadbtn_Click);
+            // 
+            // convertbtn
+            // 
+            this.convertbtn.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertbtn.Location = new System.Drawing.Point(713, 53);
+            this.convertbtn.Name = "convertbtn";
+            this.convertbtn.Size = new System.Drawing.Size(75, 40);
+            this.convertbtn.TabIndex = 1;
+            this.convertbtn.Text = "Convert";
+            this.convertbtn.UseVisualStyleBackColor = true;
+            this.convertbtn.Click += new System.EventHandler(this.convertbtn_Click);
+            // 
+            // savebtn
+            // 
+            this.savebtn.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savebtn.Location = new System.Drawing.Point(713, 93);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(75, 40);
+            this.savebtn.TabIndex = 2;
+            this.savebtn.Text = "Save";
+            this.savebtn.UseVisualStyleBackColor = true;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // updown
+            // 
+            this.updown.Location = new System.Drawing.Point(711, 138);
+            this.updown.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.updown.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.updown.Minimum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            -2147483648});
+            this.updown.Name = "updown";
+            this.updown.Size = new System.Drawing.Size(87, 20);
+            this.updown.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 711);
+            this.Controls.Add(this.updown);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.convertbtn);
+            this.Controls.Add(this.loadbtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.updown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button loadbtn;
+        private System.Windows.Forms.Button convertbtn;
+        private System.Windows.Forms.Button savebtn;
+        public System.Windows.Forms.NumericUpDown updown;
+    }
+}
+
---
+++ Image_Processing/C#/Contrast/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Brightness
+{
+    public partial class Form1 : Form
+    {
+        /* Defining Variables:
+         * Image >> to Store the Required Image.
+         * ImageData >> to store The Image Data In the Memory
+         * buffer >> buffering array used to edit the Image Data and to return back the edited ones
+         * r,g,b >> to hold the rgb values
+         * contrast >> to hold the contrast value
+         * factor >> to hold calculated contrast factor
+         * pointer >> to hold the address to the blue value of the first pixel in the memory
+         */
+        private Bitmap Image;
+        private BitmapData ImageData;
+        private byte[] buffer;
+        private int r,g,b,contrast;
+        private float factor;
+        private IntPtr pointer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /* Saving the Image file:
+         * type the file name followed by the file extension (for example new.jpg)
+         */
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(sfd.FileName);
+            }
+        }
+        /* Loading the Image file
+         * Showing it in the picturebox
+         */
+        private void loadbtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Image = new Bitmap(ofd.FileName);
+            }
+            pictureBox1.Image = (Bitmap)Image.Clone();
+        }
+        /* Converting The Image file:
+         * 1-Lock the Image Bits in the memory (PixelFormat.Format24bppRgb means that the program is going to lock only red , green and blue without including the alpha channel)
+         * 2-initializing the buffer array it's going to have all the image data (the image have height and width which leads to total pixel count = height * width and each pixel have r,g,b so the array length = height*width*3)
+         * 3-set the pointer to the location of the blue value of the first pixel of the image
+         * 4-copy the Image Data to the Buffer Array
+         * 5-Loop through each pixel and make the loop step = 3 (i+=3)
+         * 6-Calculate factor foreach pixel
+         * 7-calculate the new value for each channel
+         * 8-copy back the image Data from buffer to Image using the same pointer location
+         * 9-unlock the image bits
+         */
+        private void convertbtn_Click(object sender, EventArgs e)
+        {
+            /* Important Note:
+             * we are using buffer_image in order to not making any changes to the orignal image
+             * so we are making new image out of applying contrast vlue to the orignal one
+             * then returning the new image to picturebox
+             */
+            using (Bitmap buffer_image = (Bitmap)Image.Clone())  
+            {
+                contrast = (int)updown.Value;
+                ImageData = buffer_image.LockBits(new Rectangle(0, 0, Image.Width, Image.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+                buffer = new byte[3 * Image.Width * Image.Height];
+                pointer = ImageData.Scan0;
+                Marshal.Copy(pointer, buffer, 0, buffer.Length);
+                for (int i = 0; i < Image.Height * 3 * Image.Width; i += 3)
+                {
+                    factor = (259 * ((float)contrast + 255)) / (255 * (259 - (float)contrast));
+                    b = (int)(factor * (buffer[i] - 128) + 128);
+                    g = (int)(factor * (buffer[i + 1] - 128) + 128);
+                    r = (int)(factor * (buffer[i + 2] - 128) + 128);
+                    if (b > 255) b = 255;
+                    else if (b < 0) b = 0;
+                    if (g > 255) g = 255;
+                    else if (g < 0) g = 0;
+                    if (r > 255) r = 255;
+                    else if (r < 0) r = 0;
+                    buffer[i] = (byte)b;
+                    buffer[i + 1] = (byte)g;
+                    buffer[i + 2] = (byte)r;
+                }
+                Marshal.Copy(buffer, 0, pointer, buffer.Length);
+                buffer_image.UnlockBits(ImageData);
+                pictureBox1.Image = (Bitmap)buffer_image.Clone();
+            }
+        }
+    }
+}
